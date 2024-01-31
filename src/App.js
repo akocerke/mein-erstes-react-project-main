c5748921e48dfc4b5454a926e8c2038c06567009
akocerke <152269613+akocerke@users.noreply.github.com>
2024-01-31 11:05:35 +0100

    react-icons install css update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigationbar';
 import Footer from './components/Footer/Footer';
 import Gallerie from './components/Gallerie/Gallerie';
+import { RiAliensLine } from "react-icons/ri";
 
 import './styles/App.css';
 
@@ -15,6 +16,7 @@ function App() {
       
       <section>
         <h2>GrinsGam: Instagram mal anders</h2>
+        <RiAliensLine style={{ color: "white", fontSize: "10em" }} />
 
         <p>
           Hier gehts bald richtig ab!
