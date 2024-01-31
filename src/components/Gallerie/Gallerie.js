0e3410cb0c4cd0deebc482cb69f3866ce686ecc4
akocerke <152269613+akocerke@users.noreply.github.com>
2024-01-31 16:19:40 +0100

    Update

--- src/components/Gallerie/Gallerie.js
+++ src/components/Gallerie/Gallerie.js
@@ -29,18 +29,28 @@ const Gallery = () => {
         '/img/4.png',
         '/img/5.png',
         '/img/6.png',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
         '/img/1.png',
         '/img/2.png',
         '/img/6.png',
         '/img/4.png',
         '/img/5.png',
         '/img/6.png',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
         '/img/1.png',
         '/img/2.png',
         '/img/6.png',
         '/img/4.png',
         '/img/5.png',
-        '/img/6.png'
+        '/img/6.png',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/1.png'
         // ... weitere Bild-URLs
       ]
     };
